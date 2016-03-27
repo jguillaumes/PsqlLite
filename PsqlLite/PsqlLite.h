655e3f811ae46d51329931a51ac5db3069236cda
jguillaumes <jguillaumes@gmail.com>
2016-03-27 14:57:26 +0200

    Initial Commit

---
+++ PsqlLite/PsqlLite.h
@@ -0,0 +1,19 @@
+//
+//  PsqlLite.h
+//  PsqlLite
+//
+//  Created by Jordi Guillaumes Pons on 27/3/16.
+//  Copyright © 2016 Jordi Guillaumes Pons. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PsqlLite.
+FOUNDATION_EXPORT double PsqlLiteVersionNumber;
+
+//! Project version string for PsqlLite.
+FOUNDATION_EXPORT const unsigned char PsqlLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PsqlLite/PublicHeader.h>
+
+
